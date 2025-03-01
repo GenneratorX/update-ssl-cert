4957e58e495d419636d916e5444bd280b2c50dbc
Gennerator <gennerator@gennerator.com>
2025-03-01 16:59:49 +0200

    Refactor generate_cert.sh
    * Create variables for paths, file names, and private key type
    * Change working directory structure
    * Check if necessary files/directories exist and create them if required
      before running main script logic
    * Use single and double quotes for all command parameters and variables
      where applicable
    * Replace certbot config file with certbot command parameters
    * Add -o pipefail to set builtin

--- generate_cert.sh
+++ generate_cert.sh
@@ -1,77 +1,105 @@
 #!/bin/bash
-set -eu
+set -euo pipefail
 
-LOG_PATH="/var/log/cert-renew.log"
+LOG_PATH='/var/log/cert-renew.log'
+TMP_PATH="$PWD/tmp"
+CERT_PATH="$PWD/backup"
+CONF_PATH="$PWD/config"
+
+PRIVATE_KEY_TYPE='prime256v1'
+
+PRIVATE_KEY_NAME='letsencryptPrivkey.pem'
+CERT_KEY_NAME='letsencryptCert.pem'
+CHAIN_KEY_NAME='letsencryptChain.pem'
+FULLCHAIN_KEY_NAME='letsencryptFullchain.pem'
 
 # Print STDOUT and STDERR to screen and log both to a file
 exec > >(tee -a "$LOG_PATH") 2>&1
 
 echo "# Started script execution at $(date --iso-8601=seconds) #"
 
-if [ ! -d tmp ]; then
-        echo "========> Creating 'tmp' directory"
-        mkdir -m 700 tmp
+if [ ! -d "$TMP_PATH" ]; then
+        echo "========> Creating temp directory"
+        install -m 700 -d "$TMP_PATH"
 else
-        echo "========> Cleaning 'tmp' directory"
-        rm -f tmp/*
+        echo "========> Cleaning temp directory"
+        rm -f "$TMP_PATH/*"
+fi
+
+if [ ! -d "$CERT_PATH" ]; then
+        echo "========> Creating backup certificate directory"
+        install -m 700 -d "$CERT_PATH" "$CERT_PATH/cert_file"
+fi
+
+if [ ! -d "$CONF_PATH" ]; then
+        echo "========> Creating configuration directory"
+        install -m 700 -d "$CONF_PATH" "$CONF_PATH/certbot" "$CONF_PATH/csr_config"
+        echo "[ERROR] Please add CSR config files to '$CONF_PATH/csr_config' and 'cloudflare.conf' containing a CloudFlare API key to '$CONF_PATH'"
+        exit 1
+fi
+
+if [ ! -f "$CONF_PATH/cloudflare.conf" ]; then
+        echo "[ERROR] 'cloudflare.conf' not found in '$CONF_PATH'"
+        exit 1
+fi
+
+if [ ! -f "$CONF_PATH/csr_config/openssl.conf" ]; then
+        echo "[ERROR] 'openssl.conf' not found in '$CONF_PATH/csr_config'"
+        exit 1
 fi
 
 echo "========> Generating private key"
-openssl ecparam -genkey -name prime256v1 -check -out tmp/letsencryptPrivkey.pem
+openssl ecparam -genkey -name "$PRIVATE_KEY_TYPE" -check -out "$TMP_PATH/$PRIVATE_KEY_NAME"
 
 echo "========> Verifying private key"
-openssl ec -text -noout -check -in tmp/letsencryptPrivkey.pem
+openssl ec -text -noout -check -in "$TMP_PATH/$PRIVATE_KEY_NAME"
 
 echo "========> Generating Certificate Signing Request (CSR)"
-openssl req -new -key tmp/letsencryptPrivkey.pem -config certbot/openssl.conf -out tmp/csr -verify -verbose
+openssl req -new -key "$TMP_PATH/$PRIVATE_KEY_NAME" -config "$CONF_PATH/csr_config/openssl.conf" -out "$TMP_PATH/csr" -verify -verbose
 
 echo "========> Verifying CSR"
-openssl req -text -noout -verify -in tmp/csr
+openssl req -text -noout -verify -in "$TMP_PATH/csr"
 
 # Activate VENV
 . .venv/bin/activate
 
 echo "========> Obtaining SSL certificate from LetsEncrypt using CertBot"
-cd tmp
+cd "$TMP_PATH"
 certbot certonly \
-        --config ../certbot/certbot.conf \
-        --csr csr \
+        --config-dir "$CONF_PATH/certbot/config" \
+        --work-dir "$CONF_PATH/certbot/work" \
+        --logs-dir "$CONF_PATH/certbot/logs" \
+        --csr 'csr' \
         --dns-cloudflare \
-        --dns-cloudflare-credentials ../certbot/cloudflare.conf \
+        --dns-cloudflare-credentials "$CONF_PATH/cloudflare.conf" \
         --dns-cloudflare-propagation-seconds 30
 
-current_timestamp=$(date +%s)
+current_timestamp="$(date +%s)"
 
 echo "========> Renaming certificate files"
-echo "0000_cert.pem  -> letsencryptCert.pem.$current_timestamp"
-mv -f 0000_cert.pem "letsencryptCert.pem.$current_timestamp"
-echo "0000_chain.pem -> letsencryptChain.pem"
-mv -f 0000_chain.pem letsencryptChain.pem
-echo "0001_chain.pem -> letsencryptFullchain.pem"
-mv -f 0001_chain.pem letsencryptFullchain.pem
+echo "0000_cert.pem  -> $CERT_KEY_NAME.$current_timestamp"
+mv -f '0000_cert.pem' "$CERT_KEY_NAME.$current_timestamp"
+echo "0000_chain.pem -> $CHAIN_KEY_NAME"
+mv -f '0000_chain.pem' "$CHAIN_KEY_NAME"
+echo "0001_chain.pem -> $FULLCHAIN_KEY_NAME"
+mv -f '0001_chain.pem' "$FULLCHAIN_KEY_NAME"
 
 echo "========> Fixing file permissions"
-chmod 400 "letsencryptCert.pem.$current_timestamp" letsencryptChain.pem letsencryptFullchain.pem letsencryptPrivkey.pem
-
-if [ ! -d ../cert ]; then
-        echo "========> Creating 'cert' directory"
-        mkdir -m 700 ../cert
-        mkdir -m 700 ../cert/certificate_file
-fi
+chmod 400 "$CERT_KEY_NAME.$current_timestamp" "$CHAIN_KEY_NAME" "$FULLCHAIN_KEY_NAME" "$PRIVATE_KEY_NAME"
 
 echo "========> Packing certificate files with file name 'letsencrypt.$current_timestamp.tar.xz'"
-XZ_OPT=-e9 tar -Jcvf "../cert/letsencrypt.$current_timestamp.tar.xz" letsencryptPrivkey.pem letsencryptChain.pem letsencryptFullchain.pem --remove-files
-chmod 400 "../cert/letsencrypt.$current_timestamp.tar.xz"
+XZ_OPT=-e9 tar -Jcvf "$CERT_PATH/letsencrypt.$current_timestamp.tar.xz" "$PRIVATE_KEY_NAME" "$CHAIN_KEY_NAME" "$FULLCHAIN_KEY_NAME" --remove-files
+chmod 400 "$CERT_PATH/letsencrypt.$current_timestamp.tar.xz"
 
-echo "========> Saving main certificate file to 'cert/certificate_file' directory with file name 'letsencryptCert.pem.$current_timestamp'"
-mv -n "letsencryptCert.pem.$current_timestamp" ../cert/certificate_file/
+echo "========> Saving main certificate file with file name '$CERT_KEY_NAME.$current_timestamp'"
+mv -n "$CERT_KEY_NAME.$current_timestamp" "$CERT_PATH/cert_file/"
 
 echo "========> Cleaning up"
 rm -f ./*
 
 echo "========> Updating TLSA DNS records"
 cd ..
-./update_tlsa.py --cert_path "./cert/certificate_file/letsencryptCert.pem.$current_timestamp" --h3
+./update_tlsa.py --cert_path "$CERT_PATH/cert_file/$CERT_KEY_NAME.$current_timestamp" --h3
 
 # Deactivate VENV
 deactivate
